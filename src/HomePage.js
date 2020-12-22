689ed88fd8b84a33c473f8dd66715d717dd1b9ad
Kn0wn-Un <naikdarsh175@gmail.com>
2020-12-22 11:32:47 +0530

    homepage

--- src/HomePage.js
+++ src/HomePage.js
@@ -2,16 +2,32 @@ import { Link } from 'react-router-dom';
 import './styles.css';
 function HomePage() {
     return (
-        <div className="navigation">
-            <h2>Cyberware</h2>
-            <ul className="navbar">
+        <div>
+            <div className="navigation">
+                <h2>Cyberware</h2>
+                <ul className="navbar">
+                    <Link to="/shop">
+                        <li>Shop</li>
+                    </Link>
+                    <Link to="/cart">
+                        <li>Cart</li>
+                    </Link>
+                </ul>
+            </div>
+            <div className="description">
+                <div className="tagline">
+                    <b>Style</b> is everything.
+                </div>
+                <div className="summary">
+                    The possession of trendy cyberware has become an integral
+                    and defining part of Night City culture. Uniqueness is just
+                    another form of currency. To make it big, you need to look
+                    the part.
+                </div>
                 <Link to="/shop">
-                    <li>Shop</li>
+                    <div>Shop now!</div>
                 </Link>
-                <Link to="/cart">
-                    <li>Cart</li>
-                </Link>
-            </ul>
+            </div>
         </div>
     );
 }
